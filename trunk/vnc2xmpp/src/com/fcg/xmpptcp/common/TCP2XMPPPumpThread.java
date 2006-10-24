a7573790608df08ddb30e3efc82ec7867dacd793
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2006-10-24 07:17:34 +0000

    refactoring...

---
+++ trunk/vnc2xmpp/src/com/fcg/xmpptcp/common/TCP2XMPPPumpThread.java
@@ -0,0 +1,46 @@
+package com.fcg.xmpptcp.common;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.Socket;
+
+import org.apache.commons.codec.binary.Hex;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.XMPPConnection;
+
+public class TCP2XMPPPumpThread extends Thread {
+	private Socket socket = null;
+	private Chat chat;
+
+	public TCP2XMPPPumpThread(Socket socket, Chat chat) {
+		this.socket = socket;
+		this.chat = chat;
+	}
+
+	public void run() {
+		XMPPConnection.DEBUG_ENABLED = true;
+		try {
+			InputStream is = this.socket.getInputStream();
+			InputStream bis = new BufferedInputStream(is);
+			chat.sendMessage("_start");
+			byte[] buffer = new byte[4*1024];
+			int read;
+			do {
+				read = bis.read(buffer);
+				if (read > 0) {
+					byte[] newArray = (byte[]) Array.newInstance(buffer
+							.getClass().getComponentType(), read);
+					System.arraycopy(buffer, 0, newArray, 0, read);
+					char[] text = Hex.encodeHex(newArray);
+					chat.sendMessage(new String(text));
+				}
+			} while (read >= 0);
+			chat.sendMessage("_end");
+			bis.close();
+			is.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ trunk/vnc2xmpp/src/com/fcg/xmpptcp/common/XMPP2TCPPumpThread.java
@@ -0,0 +1,33 @@
+package com.fcg.xmpptcp.common;
+
+import java.io.OutputStream;
+import java.net.Socket;
+
+import org.apache.commons.codec.binary.Hex;
+import org.jivesoftware.smack.Chat;
+
+public class XMPP2TCPPumpThread extends Thread  {
+	private Socket socket = null;
+	private Chat chat = null;
+
+	public XMPP2TCPPumpThread(Chat chat, Socket socket) {
+		this.socket = socket;
+		this.chat = chat;
+	}
+
+	public void run() {
+		try {
+			OutputStream os = this.socket.getOutputStream();
+			do {
+				String message = chat.nextMessage().getBody();
+				if (message.equals("_start")) continue;
+				if (message.equals("_end")) break;
+				byte[] bytes = Hex.decodeHex(message.toCharArray());
+				os.write(bytes);
+			} while (true);
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
