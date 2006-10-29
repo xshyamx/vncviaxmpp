245e7593f368902eb1583b35960cfbb7f536a62f
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2006-10-29 16:45:32 +0000

    Try to improve speed

---
+++ trunk/vnc2xmpp/src/com/fcg/xmpptcp/common/ConnectionUtil.java
@@ -0,0 +1,34 @@
+package com.fcg.xmpptcp.common;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.jivesoftware.smack.SSLXMPPConnection;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+public class ConnectionUtil {
+	private static boolean isOpen(String host, int port) {
+		boolean open = false;
+		try {
+			Socket sock = new Socket();
+			sock.bind(null);
+			sock.connect(new InetSocketAddress(host, port), 1000);
+			open = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return open;
+	}
+
+	public static XMPPConnection getGoogleConnection() throws XMPPException {
+		XMPPConnection con = null;
+		if (isOpen("talk.google.com", 5222)) {
+			con = new XMPPConnection("talk.google.com", 5222, "gmail.com");
+		 } else if (isOpen("talk.google.com", 443)) {
+			 con = new SSLXMPPConnection("talk.google.com", 443, "gmail.com");
+		}
+		
+		return con;
+	}
+}
