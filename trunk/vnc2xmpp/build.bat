138a52b668df445ed1611f29619271d1a7d30605
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2006-10-29 14:39:52 +0000

    new build file

---
+++ trunk/vnc2xmpp/build.bat
@@ -0,0 +1,12 @@
+set JAVA_HOME=D:\programs\jdk1.6.0
+set PATH=%PATH%;%JAVA_HOME%\bin
+set CLASSPATH=%CLASSPATH%;bin;lib\smack.jar;;lib\smackx.jar;lib\smackx-debug.jar
+
+mkdir bin
+
+javac -d bin src\com\fcg\xmpptcp\common\TCP2XMPPPumpThread.java 
+javac -d bin src\com\fcg\xmpptcp\common\XMPP2TCPPumpThread.java
+javac -d bin src\com\fcg\xmpptcp\tcp2xmpp\TCPOverXMPPThread.java
+javac -d bin src\com\fcg\xmpptcp\tcp2xmpp\TCP2XMPPMain.java
+javac -d bin src\com\fcg\xmpptcp\xmpp2tcp\XMPPOverTCPThread.java
+javac -d bin src\com\fcg\xmpptcp\xmpp2tcp\XMPP2TCPMain.java
