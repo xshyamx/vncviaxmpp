449d5f801b88913ff51a5257f59f2f669d2bbbaf
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2006-10-30 11:21:23 +0000

    add support for select username to login

--- trunk/vnc2xmpp/src/com/fcg/xmpptcp/tcp2xmpp/TCP2XMPPMain.java
+++ trunk/vnc2xmpp/src/com/fcg/xmpptcp/tcp2xmpp/TCP2XMPPMain.java
@@ -9,7 +9,7 @@ public class TCP2XMPPMain {
 
 	/**
 	 * http://java.sun.com/docs/books/tutorial/networking/sockets/clientServer.html
-	 * 
+	 *
 	 * @param args
 	 * @throws XMPPException
 	 * @throws IOException
@@ -17,7 +17,34 @@ public class TCP2XMPPMain {
 	public static void main(String[] args) throws XMPPException, IOException {
 		ServerSocket serverSocket = null;
 		boolean listening = true;
-		String port = args.length > 0 ? args[0] : "5900";
+
+		String port = null;
+		String username = null;
+		String password = null;
+
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			try {
+				int p = Integer.parseInt(arg);
+				port = String.valueOf(p);
+			} catch (Exception e) {
+				if (username == null) {
+					username = arg;
+					if (username.indexOf('@') >= 0) {
+						username = username.substring(0, username.indexOf('@'));
+					}
+				} else {
+					password = arg;
+				}
+			}
+		}
+		if (port == null) port = "5900";
+		if (username == null) username = "xmppclient";
+		if (password == null) password = "thaiha";
+		
+		System.out.println("Listen port:" + port + "; username: " + username + "; password:" + password);
+
+
 		try {
 			serverSocket = new ServerSocket(Integer.parseInt(port));
 		} catch (IOException e) {
@@ -27,7 +54,7 @@ public class TCP2XMPPMain {
 		//usr/pwd: xmppclient@gmail.com/thaiha
 		//usr/pwd: xmppserver@gmail.com/thaiha
 		while (listening) {
-			new TCPOverXMPPThread(serverSocket.accept(), "xmppclient", "thaiha", "xmppserver@gmail.com").start();
+			new TCPOverXMPPThread(serverSocket.accept(), username, password, "xmppserver@gmail.com").start();
 		}
 		serverSocket.close();
 	}
