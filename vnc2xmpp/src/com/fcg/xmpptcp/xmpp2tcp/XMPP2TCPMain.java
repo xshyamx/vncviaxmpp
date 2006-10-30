14c5eaf2a96af6f302628ddc89e4193a048e1059
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2006-10-30 10:33:40 +0000

    Implement Reconnect

--- vnc2xmpp/src/com/fcg/xmpptcp/xmpp2tcp/XMPP2TCPMain.java
+++ vnc2xmpp/src/com/fcg/xmpptcp/xmpp2tcp/XMPP2TCPMain.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 import java.util.Hashtable;
 
 import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ConnectionListener;
 import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
@@ -26,14 +27,31 @@ public class XMPP2TCPMain {
 
 	private static Hashtable threadIds = new Hashtable();
 
-	public static void main(String[] args) throws XMPPException {
+	private static String[] args;
+
+	public static void main(String[] args) throws XMPPException{
+		/* save this value to be used when relaunching server */
+		XMPP2TCPMain.args = args;
+
 		final String host = args.length > 1 ? args[0] : "localhost";
 		final String port = args.length > 1 ? args[1] : (args.length == 1 ? args[0] : "5900");
-		
+
 		// If we remove this, the main thread will exit and the background threads exit too --> server stops immediately
 		XMPPConnection.DEBUG_ENABLED = true;
 
-		con = ConnectionUtil.getGoogleConnection();//new SSLXMPPConnection("talk.google.com", 443, "gmail.com");
+		/* try to get the connection, if failed, wait 1s and try again until we success */
+		long waitTime = 1000;
+		do {
+			con = ConnectionUtil.getGoogleConnection();//new SSLXMPPConnection("talk.google.com", 443, "gmail.com");
+			if (con == null) {
+				try {
+					Thread.sleep(waitTime);
+					waitTime *= 2; // next time, wait a longer time
+				} catch (Exception e) {
+				}
+			}
+		} while (con == null);
+
 		con.login(gtalkUsr, gtalkPwd);
 
 		// Accept only messages from client
@@ -46,14 +64,26 @@ public class XMPP2TCPMain {
 				try {
 					if (packet instanceof Message) {
 						Message msg = (Message) packet;
+						boolean existConnection;
 						synchronized(threadIds) {
 							if (!threadIds.containsKey(msg.getThread())) {
+								existConnection = false;
 								threadIds.put(msg.getThread(), "true");
+							} else {
+								existConnection = true;
+							}
+						}
+						if (!existConnection) {
+							Socket socket;
+							try {
+								socket = new Socket(host, Integer.parseInt(port));
 								/* a message of a brand new thread */
 								Chat chat = new Chat(con, "xmppclient@gmail.com", msg.getThread());
-								Socket socket = new Socket(host, Integer.parseInt(port));
 								System.out.println("Chat not exist, create new tunnel " + msg.getThread());
 								new XMPPOverTCPThread(socket, chat).start();
+							} catch (Exception e) {
+								/* cannot open a socket? inform the sender*/
+								new Chat(con, "xmppclient@gmail.com", msg.getThread()).sendMessage("0:_end");
 							}
 						}
 					}
@@ -67,23 +97,28 @@ public class XMPP2TCPMain {
 		con.addPacketListener(myListener, filter);
 
 		// Verify this code to make sure we can relaunch this server when error happens
-		/*
 		// Register connection listener
-		ConnectionListener conListener = new ConnectionListener() {
+		con.addConnectionListener(new ConnectionListener() {
 
 			public void connectionClosed() {
 			}
 
 			public void connectionClosedOnError(Exception e) {
+				/* cleanup */
 				try {
+					System.out.println("RESTARTING....");
 					threadIds.clear();
 					con.close();
 				} catch (Exception e2) {
 				}
+
+				/* sleep 10s because the network may not be stable soon */
 				try {
-					wait(10000);
-				} catch (Exception e3) {
+					Thread.sleep(10000);
+				} catch (Exception e2) {
 				}
+
+				/* relaunch the main flow */
 				Thread t = new Thread() {
 					public void run() {
 						try {
@@ -94,8 +129,6 @@ public class XMPP2TCPMain {
 				};
 				t.start();
 			}
-		};
-		con.addConnectionListener(conListener);
-		*/
+		});
 	}
 }
