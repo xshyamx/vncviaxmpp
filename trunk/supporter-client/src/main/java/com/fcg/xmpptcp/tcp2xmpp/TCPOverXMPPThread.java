b97e421fb68769d1a1edfd6b18a272a01574a459
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2007-04-13 15:48:30 +0000

    Initial setup

---
+++ trunk/supporter-client/src/main/java/com/fcg/xmpptcp/tcp2xmpp/TCPOverXMPPThread.java
@@ -0,0 +1,45 @@
+package com.fcg.xmpptcp.tcp2xmpp;
+
+import java.net.Socket;
+import java.util.Date;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+import com.fcg.xmpptcp.common.ConnectionUtil;
+import com.fcg.xmpptcp.common.TCP2XMPPPumpThread;
+import com.fcg.xmpptcp.common.XMPP2TCPPumpThread;
+
+public class TCPOverXMPPThread extends Thread {
+	private Socket socket = null;
+	private XMPPConnection connection = null;
+	private Chat chat;
+	private String resource = "TCPOverXMPPThread_" + (new Date()).getTime();
+	
+	public TCPOverXMPPThread(Socket socket, String gtalkUsr, String gtalkPwd, String recipientId) throws XMPPException {
+		super("TCPOverXMPPThread");
+		this.socket = socket;
+		XMPPConnection con = ConnectionUtil.getGoogleConnection(); //new SSLXMPPConnection("talk.google.com", 443, "gmail.com");
+		con.login(gtalkUsr, gtalkPwd, resource);
+		this.connection = con;
+		chat = connection.createChat(recipientId);
+	}
+ 
+	public void run() {
+		try {
+			Thread t1 = new TCP2XMPPPumpThread(socket, chat);
+			Thread t2 = new XMPP2TCPPumpThread(chat, socket);
+			t1.start();
+			t2.start();
+						
+			t1.join();
+			t2.join();
+			socket.close();
+			connection.close();
+			System.out.println("CLOSED connection of resource :" + resource);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ trunk/supporter-server/src/main/java/com/fcg/xmpptcp/tcp2xmpp/TCP2XMPPMain.java
@@ -0,0 +1,67 @@
+package com.fcg.xmpptcp.tcp2xmpp;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.jivesoftware.smack.XMPPException;
+
+import com.fcg.xmpptcp.common.VNCUtils;
+
+public class TCP2XMPPMain {
+
+	/**
+	 * http://java.sun.com/docs/books/tutorial/networking/sockets/clientServer.html
+	 *
+	 * @param args
+	 * @throws XMPPException
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws XMPPException, IOException {
+		ServerSocket serverSocket = null;
+		boolean listening = true;
+
+		String port = null;
+		String username = null;
+		String password = null;
+
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			try {
+				int p = Integer.parseInt(arg);
+				port = String.valueOf(p);
+			} catch (Exception e) {
+				if (username == null) {
+					username = arg;
+					if (username.indexOf('@') >= 0) {
+						username = username.substring(0, username.indexOf('@'));
+					}
+				} else {
+					password = arg;
+				}
+			}
+		}
+		if (port == null) port = "5900";
+		if (username == null) username = "xmppclient";
+		if (password == null) password = "thaiha";
+		
+		System.out.println("Listen port:" + port + "; username: " + username + "; password:" + password);
+
+
+		try {
+			serverSocket = new ServerSocket(Integer.parseInt(port));
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: " + port);
+			System.exit(-1);
+		}
+		
+		//start vnc client
+		VNCUtils.startVNCClient("localhost", Integer.parseInt(port));
+
+		//usr/pwd: xmppclient@gmail.com/thaiha
+		//usr/pwd: xmppserver@gmail.com/thaiha
+		while (listening) {
+			new TCPOverXMPPThread(serverSocket.accept(), username, password, "xmppserver@gmail.com").start();
+		}
+		serverSocket.close();
+	}
+}
