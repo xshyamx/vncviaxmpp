eb6bbcb013e8099cf64629f611a232c50021d6ef
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2008-02-06 13:59:33 +0000

    add error message

--- javavnc/client.bat
+++ javavnc/client.bat
@@ -1,2 +1,6 @@
 REM VNC password: 123
-java -cp .\javavnc-1.0-SNAPSHOT.jar;target\dependency\javavnc-1.0-SNAPSHOT.jar org.javavnc.tcp2xmpp.TCP2XMPPMain
+@echo off
+cmd /c java -cp .\javavnc-1.0-SNAPSHOT.jar;target\dependency\javavnc-1.0-SNAPSHOT.jar org.javavnc.tcp2xmpp.TCP2XMPPMain
+IF NOT ERRORLEVEL 1 GOTO end
+echo Please install Java Runtime Environment (JRE) version 5 or later from http://java.sun.com/javase/downloads/index.jsp
+:end
