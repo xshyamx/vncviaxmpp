34012de5aeeb3f5f87572999382ea92998567f41
hathanhthai <hathanhthai@23eebbd6-6620-0410-a8be-494cc8ed8415>
2008-02-06 13:59:33 +0000

    add error message

--- trunk/javavnc/server.bat
+++ trunk/javavnc/server.bat
@@ -1,2 +1,8 @@
-rem java -server -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -cp .\javavnc-1.0-SNAPSHOT.jar;target\dependency\javavnc-1.0-SNAPSHOT.jar org.javavnc.xmpp2tcp.XMPP2TCPMain
-java -cp .\javavnc-1.0-SNAPSHOT.jar;target\dependency\javavnc-1.0-SNAPSHOT.jar org.javavnc.xmpp2tcp.XMPP2TCPMain
+@echo off
+
+cmd /c java -server -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -cp .\javavnc-1.0-SNAPSHOT.jar;target\dependency\javavnc-1.0-SNAPSHOT.jar org.javavnc.xmpp2tcp.XMPP2TCPMain
+rem cmd /c  java -cp .\javavnc-1.0-SNAPSHOT.jar;target\dependency\javavnc-1.0-SNAPSHOT.jar org.javavnc.xmpp2tcp.XMPP2TCPMain
+
+IF NOT ERRORLEVEL 1 GOTO end
+echo Please install Java Runtime Environment (JRE) version 5 or later from http://java.sun.com/javase/downloads/index.jsp
+:end
